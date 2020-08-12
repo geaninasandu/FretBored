3a7ac04aed40e794421412b37e7a96e6983293fe
Geanina Sandu <sandugeanina83@gmail.com>
2020-08-12 16:52:14 +0300

    Refactor js files.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
